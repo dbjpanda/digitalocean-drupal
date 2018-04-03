97bcfee71e0e6af3f64f6a67e29e17cbb1cc070d
Dibyajyoti Panda <dbjpanda@live.com>
2018-04-03 12:09:17 +0530

    Update install.sh
    drupal 7 init

--- install.sh
+++ install.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-sudo apt-get purge php.*
-sudo rm -rf /etc/php
 
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
-
-sudo apt-get install php7.2
-sudo apt-get install php7.2-fpm
-
-sed -i -e 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.2/cli/php.ini
-sed -i -e 's/listen = \/run\/php\/php7.2-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/7.2/fpm/pool.d/www.conf
 
 truncate -s 0 /etc/nginx/sites-available/digitalocean
 cat > /etc/nginx/sites-available/digitalocean<<'EOF'
@@ -70,33 +60,17 @@ server {
         return 404;
     }
 
-    # In Drupal 8, we must also match new paths where the '.php' appears in
-    # the middle, such as update.php/selection. The rule we use is strict,
-    # and only allows this pattern with the update.php front controller.
-    # This allows legacy path aliases in the form of
-    # blog/index.php/legacy-path to continue to route to Drupal nodes. If
-    # you do not have any paths like that, then you might prefer to use a
-    # laxer rule, such as:
-    #   location ~ \.php(/|$) {
-    # The laxer rule will continue to work if Drupal uses this new URL
-    # pattern with front controllers other than update.php in a future
-    # release.
+    
     location ~ '\.php$|^/update.php' {
         fastcgi_split_path_info ^(.+?\.php)(|/.*)$;
-        # Security note: If you're running a version of PHP older than the
-        # latest 5.3, you should have "cgi.fix_pathinfo = 0;" in php.ini.
-        # See http://serverfault.com/q/627903/94922 for details.
         include fastcgi_params;
-        # Block httpoxy attacks. See https://httpoxy.org/.
         fastcgi_param HTTP_PROXY "";
         fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
         fastcgi_param PATH_INFO $fastcgi_path_info;
         fastcgi_param QUERY_STRING $query_string;
         fastcgi_intercept_errors on;
-        # PHP 5 socket location.
-        #fastcgi_pass unix:/var/run/php5-fpm.sock;
-        # PHP 7 socket location.
-        fastcgi_pass 127.0.0.1:9000;
+        fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
+        
     }
 
     # Fighting with Styles? This little gem is amazing.
@@ -124,7 +98,7 @@ server {
            location ~ ^/phpmyadmin/(.+\.php)$ {
                    try_files $uri =404;
                    root /usr/share/;
-                   fastcgi_pass 127.0.0.1:9000;
+                   fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
                    fastcgi_index index.php;
                    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
                    include /etc/nginx/fastcgi_params;
@@ -133,18 +107,19 @@ server {
                    root /usr/share/;
            }
     }
+
     
 }
 
 EOF
 
 
-sudo apt-get install php7.2-dom php7.2-gd php7.2-mysql
+sudo apt-get install php7.0-dom php7.0-gd php7.0-mbstring
 cd /var/www/html
-wget https://ftp.drupal.org/files/projects/drupal-8.5.0.tar.gz
+wget https://ftp.drupal.org/files/projects/drupal-7.58.tar.gz
 tar xzvf drupal*
 mkdir drupal
-mv -v drupal-8.5.0/*  drupal/
+mv -v drupal*/*  drupal/
 mkdir /var/www/html/drupal/sites/default/files
 cp /var/www/html/drupal/sites/default/default.settings.php /var/www/html/drupal/sites/default/settings.php
 sudo chown -R :www-data /var/www/html/drupal/*
@@ -158,7 +133,6 @@ unzip phpMyAdmin-4.7.9-all-languages.zip
 mv phpMyAdmin-4.7.9-all-languages /usr/share/phpmyadmin
 
 #install composer and Drush
-sudo apt-get install php7.2-mbstring php7.2-curl
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 wget -O drush.phar https://github.com/drush-ops/drush-launcher/releases/download/0.6.0/drush.phar
 chmod +x drush.phar
